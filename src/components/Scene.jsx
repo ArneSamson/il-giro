2bd13ff59bb9fbdf0ba0f49e5fc03d9f0aaf47e5
ArneSamson <arnesamson007@gmail.com>
2024-04-02 10:09:03 +0200

    ref: update store import

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -3,7 +3,7 @@ import Sink from "./kitchen/Sink.jsx";
 import Tower from "./kitchen/Tower.jsx";
 import Table from "./kitchen/Table.jsx";
 
-import useConfig from "../store/useConfigStore.jsx";
+import useConfigStore from "../store/useConfigStore.jsx";
 
 export default function Scene() {
 
@@ -14,7 +14,7 @@ export default function Scene() {
         cooktopChosen,
         towerChosen,
         tableChosen,
-    } = useConfig((state) => ({
+    } = useConfigStore((state) => ({
         sinkChosen: state.sinkChosen,
         cooktopChosen: state.cooktopChosen,
         towerChosen: state.towerChosen,
