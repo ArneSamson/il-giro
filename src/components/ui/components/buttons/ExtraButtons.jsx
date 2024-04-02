69c4116e2286d07a83e6c35695682a933fd59dae
ArneSamson <arnesamson007@gmail.com>
2024-04-02 13:54:17 +0200

    fix: zoom out button zoomms out again

--- src/components/ui/components/buttons/ExtraButtons.jsx
+++ src/components/ui/components/buttons/ExtraButtons.jsx
@@ -40,7 +40,7 @@ export default function ExtraButtons() {
     );
 
     const handleZoom = () => {
-        if (currentPage === 0) return;
+        // if (currentPage === 0) return;
         setCameraFocus([0, 1, 0]);
         setCurrentPage(0);
 
