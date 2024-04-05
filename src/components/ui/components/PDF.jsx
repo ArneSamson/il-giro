54db56178523f91f79a8f407fc0eca16c774369d
ArneSamson <arnesamson007@gmail.com>
2024-04-05 11:31:01 +0200

    feat: style and create PDF in line with example of client

--- src/components/ui/components/PDF.jsx
+++ src/components/ui/components/PDF.jsx
@@ -1,62 +1,97 @@
 import React from 'react';
 import { Page, Text, View, Document, StyleSheet, BlobProvider, Image, Font } from '@react-pdf/renderer';
 
-Font.register({ family: 'Kaisei Decol', src: '/fonts/KaiseiDecol-Regular.ttf' });
+// Font.register({ family: 'Kaisei Decol', src: '/fonts/KaiseiDecol-Regular.ttf' });
+Font.register({ family: 'calibri', src: '/fonts/calibri.ttf' });
 
 const styles = StyleSheet.create({
     page: {
         flexDirection: 'column',
         backgroundColor: '#fafafa',
-    },
-    section: {
-        margin: 20,
-    },
-    subsection: {
-        flexGrow: 1,
-        paddingBottom: 10
-    },
-    title: {
-        fontFamily: 'Kaisei Decol',
-        fontSize: 36,
-        fontWeight: 400,
-        marginBottom: 10
-    },
-    subtitle: {
-        fontFamily: 'Kaisei Decol',
-        fontSize: 18,
-        marginBottom: 5
-    },
-    value: {
-        fontSize: 12
-    },
-
-    backgroundImage: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
+        fontSize: 11,
+        padding: 50,
         width: '100%',
+        gap: 40,
+    },
+
+    heading: {
 
     },
 
-    banner: {
-        width: '100%',
-        height: 300,
+    options: {
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 20
+        width: '100%',
+        alignContent: 'space-between',
+        gap: 20,
+
     },
-    bannerTitle: {
-        fontSize: 70,
-        fontWeight: 400,
-        color: '#272727',
-        fontFamily: 'Kaisei Decol'
+    model: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        alignContent: 'space-between'
     },
-    heroImage: {
-        width: 'auto',
-        height: 200
-    }
+    modelTitle: {
+        // fontSize: 11,
+    },
+    optionsTitle: {
+        width: 100,
+    },
+    modelOptions: {
+        display: 'flex',
+        flexDirection: 'column',
+        border: '2px solid black',
+        width: '100%', // Set width to 100%
+    },
+    modelOption: {
+        display: 'flex',
+        flexDirection: 'row',
+        borderBottom: '1px solid black',
+        padding: 5,
+    },
+
+    appliances: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        alignContent: 'space-around'
+    },
+
+    applianceOptions: {
+        display: 'flex',
+        flexDirection: 'column',
+        border: '2px solid black',
+        width: '100%', // Set width to 100%
+    },
+
+    applianceOption: {
+        display: 'flex',
+        flexDirection: 'row',
+        borderBottom: '1px solid black',
+        padding: 5,
+    },
+
+    table: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        alignContent: 'space-between',
+    },
+    row: {
+        width: '100%',
+        flexDirection: 'row',
+    },
+    cell: {
+        width: '100%',
+        border: '1px solid black',
+    },
+    cellContent: {
+        paddingLeft: 8,
+        paddingTop: 6,
+        textAlign: 'left',
+    },
+
 });
 
 export function MyDocument({ props }) {
@@ -73,136 +108,255 @@ export function MyDocument({ props }) {
             <Page size="A4" style={styles.page} wrap={true}>
 
                 <View
-                    style={styles.banner}
+                    style={styles.heading}
                 >
-                    <Text
-                        style={styles.bannerTitle}
+                    <View
+                        style={styles.table}
                     >
-                        Il Giro
-                    </Text>
-                    <Image
-                        src='/images/UI/hero.png'
-                        source={'./images/UI/hero.png'}
-                        style={styles.heroImage}
-                    />
+                        <View
+                            style={[styles.row, { width: '100%', height: 100 }]}
+                        >
+                            <View
+                                style={[styles.cell, { width: 'auto', height: 100 }]}
+                            >
+                                <Image
+                                    style={{ padding: 10 }}
+                                    src='/images/UI/Logo_SR_Black.png'
+                                    source={'./images/UI/Logo_SR_Black.png'}
+                                />
+                            </View>
+
+                            <View
+                                style={styles.cell}
+                            >
+                                <View
+                                    style={styles.row}
+                                >
+                                    <View
+                                        style={styles.cell}
+                                    >
+                                        <Text
+                                            style={styles.cellContent}
+                                        >
+                                            PURCHASE ORDER IL GIRO
+                                        </Text>
+                                    </View>
+
+                                    <View
+                                        style={styles.cell}
+                                    >
+                                        <Text
+                                            style={styles.cellContent}
+                                        >
+                                            date :
+                                        </Text>
+                                    </View>
+
+                                </View>
+                                <View
+                                    style={styles.row}
+                                >
+                                    <View
+                                        style={styles.cell}
+                                    >
+                                        <Text
+                                            style={styles.cellContent}
+                                        >
+                                            reference :
+                                        </Text>
+                                    </View>
+
+                                    <View
+                                        style={styles.cell}
+                                    >
+                                        <Text
+                                            style={styles.cellContent}
+                                        >
+
+                                            ...
+                                        </Text>
+                                    </View>
+
+                                </View>
+                            </View>
+
+                        </View>
+
+                        <View
+                            style={[styles.row, { width: '100%', height: 100 }]}
+                        >
+                            <View
+                                style={styles.cell}
+                            >
+                                <Text
+                                    style={styles.cellContent}
+                                >
+                                    customer :
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.cell}
+                            >
+                                <Text
+                                    style={styles.cellContent}
+                                >
+                                    delivery adress :
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.cell}
+                            >
+                                <Text
+                                    style={styles.cellContent}
+                                >
+                                    delivery week :
+                                </Text>
+                            </View>
+                        </View>
+
+                    </View>
+
                 </View>
 
-
-                <View style={styles.section}>
-                    <Image
-                        src='/images/UI/bg2.png'
-                        source={'./images/UI/bg2.png'}
-                        style={styles.backgroundImage}
-                    />
-
-                    <Text style={styles.title}>Overview of Your Order</Text>
-
+                <View
+                    style={styles.options}
+                >
 
                     <View
-                        style={styles.subsection}
+                        style={styles.model}
                     >
-                        <Text style={styles.subtitle}>Chosen Modules:</Text>
-                        <Text style={styles.value}>{props.chosenModules}</Text>
+                        <Text
+                            style={styles.optionsTitle}
+                        >
+                            model :
+                        </Text>
+
+                        <View
+                            style={styles.modelOptions}
+                        >
+                            <View
+                                style={styles.modelOption}
+                            >
+                                <Text>
+                                    model name : {props.chosenModules}
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.modelOption}
+                            >
+                                <Text>
+                                    material type : {props.mainMaterial.name}
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.modelOption}
+                            >
+                                <Text>
+                                    finish color :
+                                </Text>
+                            </View>
+
+                            <View
+                                style={[styles.modelOption, { borderBottom: '2px solid black' }, { borderTop: '2px solid black' }]}
+                            >
+                                <Text>
+                                    type of handle :
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.modelOption}
+                            >
+                                <Text>
+                                    ceiling panel :
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.modelOption}
+                            >
+                                <Text>
+                                    LED lighting :
+                                </Text>
+                            </View>
+
+                            <View
+                                style={[styles.modelOption, { borderBottom: 'none' }]}
+                            >
+                                <Text>
+                                    countertop : {props.tableTopMaterial.name}
+                                </Text>
+                            </View>
+
+                        </View>
+
                     </View>
 
                     <View
-                        style={styles.subsection}
+                        style={styles.appliances}
                     >
-                        <Text style={styles.subtitle}>Curved:</Text>
-                        <Text style={styles.value}>{props.bevelled}</Text>
+                        <Text
+                            style={styles.optionsTitle}
+                        >
+                            appliances :
+                        </Text>
+
+                        <View
+                            style={styles.applianceOptions}
+                        >
+                            <View
+                                style={styles.applianceOption}
+                            >
+                                <Text>
+                                    type of wine cooler:
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.applianceOption}
+                            >
+                                <Text>
+                                    type of refrigerator :
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.applianceOption}
+                            >
+                                <Text>
+                                    type of cooking fire :
+                                </Text>
+                            </View>
+
+                            <View
+                                style={styles.applianceOption}
+                            >
+                                <Text>
+                                    type of sink :
+                                </Text>
+
+                            </View>
+
+                            <View
+                                style={[styles.applianceOption, { borderBottom: 'none' }]}
+                            >
+                                <Text>
+                                    type of faucet :
+                                </Text>
+
+                            </View>
+
+                        </View>
+
                     </View>
-
-                    <View
-                        style={styles.subsection}
-                    >
-                        <Text style={styles.subtitle}>Main Material:</Text>
-                        <Text style={styles.value}>{props.mainMaterial.name}</Text>
-                    </View>
-
-                    <View
-                        style={styles.subsection}
-                    >
-                        <Text style={styles.subtitle}>Table Top Material:</Text>
-                        <Text style={styles.value}>{props.tableTopMaterial.name}</Text>
-                    </View>
-
-                    {props.accentMaterial &&
-                        <>
-                            <View
-                                style={styles.subsection}
-                            >
-                                <Text style={styles.subtitle}>Accent Material:</Text>
-                                <Text style={styles.value}>{props.accentMaterial.name}</Text>
-                            </View>
-                        </>
-                    }
-
-                    <View
-                        style={styles.subsection}
-                    >
-                        <Text style={styles.subtitle}>Edge Finish:</Text>
-                        <Text style={styles.value}>{props.edgeFinish}</Text>
-                    </View>
-
-                    {props.tapType && (
-                        <>
-                            <View
-                                style={styles.subsection}
-                            >
-                                <Text style={styles.subtitle}>Tap Type:</Text>
-                                <Text style={styles.value}>{props.tapType}</Text>
-                            </View>
-                        </>
-                    )}
-
-                    {props.mainDrawers && (
-                        <>
-                            <View
-                                style={styles.subsection}
-                            >
-                                <Text style={styles.subtitle}>Extra Drawers:</Text>
-                                <Text style={styles.value}>{props.mainDrawers}</Text>
-                            </View>
-                        </>
-                    )}
-
-
-                    {props.stoveType && (
-                        <>
-                            <View
-                                style={styles.subsection}
-                            >
-                                <Text style={styles.subtitle}>Stove Type:</Text>
-                                <Text style={styles.value}>{props.stoveType}</Text>
-                            </View>
-                        </>
-                    )}
-
-                    {props.applianceType && (
-                        <>
-                            <View
-                                style={styles.subsection}
-                            >
-                                <Text style={styles.subtitle}>Appliance Type:</Text>
-                                <Text style={styles.value}>{props.applianceType}</Text>
-                            </View>
-                        </>
-                    )}
-
-                    {props.wineStandSize && (
-                        <>
-                            <View
-                                style={styles.subsection}
-                            >
-                                <Text style={styles.subtitle}>Wine Stand Size:</Text>
-                                <Text style={styles.value}>{props.wineStandSize}</Text>
-                            </View>
-                        </>
-                    )}
 
                 </View>
             </Page>
-        </Document>
+        </Document >
     );
 };
 
