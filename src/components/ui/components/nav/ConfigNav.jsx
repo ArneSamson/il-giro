38677b31b9bd483b81c7118ac9b99da1d3798740
ArneSamson <arnesamson007@gmail.com>
2024-04-08 13:48:26 +0200

    feat: add next and back to buttons

--- src/components/ui/components/nav/ConfigNav.jsx
+++ src/components/ui/components/nav/ConfigNav.jsx
@@ -72,9 +72,10 @@ export default function ConfigNav() {
                 onClick={handleBack}
             >
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M15 8H1" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                    <path d="M8 15L1 8L8 1" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M15 8H1" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M8 15L1 8L8 1" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
+                Back
             </button>
 
             <button
@@ -82,11 +83,11 @@ export default function ConfigNav() {
                 className={currentPage === pagesAmount ? 'config-ui__nav__button--disabled' : ''}
                 onClick={handleNext}
             >
+                Next
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                    <path d="M8 1L15 8L8 15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M1 8H15" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M8 1L15 8L8 15" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
-
             </button>
         </div>
     </>
