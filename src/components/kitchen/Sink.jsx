3e975030a142e2292e5ec4187742de1a3584376b
ArneSamson <arnesamson007@gmail.com>
2024-04-08 09:58:07 +0200

    feat: update tabletop

--- src/components/kitchen/Sink.jsx
+++ src/components/kitchen/Sink.jsx
@@ -126,14 +126,11 @@ export default function Sink({ props }) {
 
                 <group
                     position={tableTopInset ? [0, 0, 0] : [0, 0.02, 0]}
+                    scale={tableTopInset ? [1, 1, 1] : [1.05, 1, 1.05]}
                 >
                     <>
                         <TableTopCutOut
-                            props={
-                                {
-                                    scale: tableTopInset ? [1, 1, 1] : [1.05, 1, 1.05],
-                                }
-                            }
+
                         />
 
                         <Reginox
