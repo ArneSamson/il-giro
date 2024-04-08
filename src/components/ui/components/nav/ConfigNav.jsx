b82dfd0bc667fa6cb378f5b01587ca7d56de014b
ArneSamson <arnesamson007@gmail.com>
2024-04-08 13:56:33 +0200

    feat: change next and previous button

--- src/components/ui/components/nav/ConfigNav.jsx
+++ src/components/ui/components/nav/ConfigNav.jsx
@@ -71,10 +71,10 @@ export default function ConfigNav() {
                 className={currentPage === 0 ? 'config-ui__nav__button--disabled' : ''}
                 onClick={handleBack}
             >
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                {/* <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15 8H1" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M8 15L1 8L8 1" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
+                </svg> */}
                 Back
             </button>
 
@@ -84,10 +84,10 @@ export default function ConfigNav() {
                 onClick={handleNext}
             >
                 Next
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                {/* <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 8H15" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M8 1L15 8L8 15" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
+                </svg> */}
             </button>
         </div>
     </>
