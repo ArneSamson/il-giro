876cbab9dded9cde1ee4f21e33d5ed53684ed222
ArneSamson <arnesamson007@gmail.com>
2024-04-15 15:21:03 +0200

    feat: different height positions for alcoohol at different sizes of winestands

--- src/components/kitchen/accessoires/WineStand.jsx
+++ src/components/kitchen/accessoires/WineStand.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei'
 
 import useConfig from '../../../store/useConfigStore.jsx';
@@ -19,6 +19,28 @@ export default function WineStand({ props }) {
             wineStandSize: state.wineStandSize,
         })
     );
+
+    const [alcoholHeight, setAlcoholHeight] = useState(1.855);
+    const [heightOffset, setHeighOffset] = useState(0.05);
+
+    useEffect(() => {
+        switch (wineStandSize) {
+            case 'tall':
+                setAlcoholHeight(1.37);
+                setHeighOffset(0.4);
+                break;
+            case 'medium':
+                setAlcoholHeight(1.55);
+                setHeighOffset(0.301);
+                break;
+            case 'small':
+                setAlcoholHeight(1.68);
+                setHeighOffset(0);
+                break;
+            default:
+                break;
+        }
+    }, [wineStandSize]);
 
     const { nodes } = useGLTF("./models/winestand.glb");
     return (
@@ -66,24 +88,24 @@ export default function WineStand({ props }) {
             }
 
             <GlassBottleWhiskey
-                position={[-0.05, 1.855, -0.05]}
+                position={[-0.08, alcoholHeight, -0.05]}
                 scale={[0.8, 0.8, 0.8]}
                 rotation={[0, 0, 0]}
             />
 
             <WineBottle
-                position={[0.05, 1.855, -0.05]}
+                position={[0.05, alcoholHeight, -0.05]}
                 scale={[0.8, 0.8, 0.8]}
                 rotation={[0, Math.PI, 0]}
             />
             <WineBottle
-                position={[0.1, 1.855, 0.05]}
+                position={[0.1, alcoholHeight + heightOffset, 0.05]}
                 scale={[0.8, 0.8, 0.8]}
                 rotation={[0, 0, 0]}
             />
 
             <Wodka
-                position={[-0.05, 1.55, 0]}
+                position={[-0.05, alcoholHeight + heightOffset, 0.1]}
                 scale={[0.8, 0.8, 0.8]}
                 rotation={[0, 0, 0]}
             />
