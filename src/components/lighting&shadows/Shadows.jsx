e29c4b82862ce326b24b9054733116e41e4fb4ed
ArneSamson <arnesamson007@gmail.com>
2024-04-22 11:49:06 +0200

    chore: remove unused components

--- src/components/lighting&shadows/Shadows.jsx
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
-import { ContactShadows } from '@react-three/drei'
-
-export default function Lights() {
-    return <>
-
-    </>
-}
