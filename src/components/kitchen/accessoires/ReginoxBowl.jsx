4f9f97a4534309bdb8c49c91ba5b558cd7a97600
ArneSamson <arnesamson007@gmail.com>
2024-04-23 10:16:25 +0200

    feat: add black plane under sink to hide inside

--- src/components/kitchen/accessoires/ReginoxBowl.jsx
+++ src/components/kitchen/accessoires/ReginoxBowl.jsx
@@ -1,59 +1,55 @@
 import React, { useState, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 
-import useConfig from '../../../store/useConfigStore.jsx';
+import useConfig from "../../../store/useConfigStore.jsx";
 
-import NewMaterial from '../../../helper/NewMaterial.jsx';
+import NewMaterial from "../../../helper/NewMaterial.jsx";
 
 export default function Reginox({ props }) {
+  const { tableTopMaterial } = useConfig((state) => ({
+    tableTopMaterial: state.tableTopMaterial,
+  }));
 
-    const {
-        tableTopMaterial,
-    } = useConfig(
-        state => ({
-            tableTopMaterial: state.tableTopMaterial,
-        })
-    );
+  const { nodes } = useGLTF(
+    tableTopMaterial.name === "inox"
+      ? "./models/Reginox-solid.glb"
+      : "./models/Reginox.glb"
+  );
 
-    const { nodes } = useGLTF(tableTopMaterial.name === "inox" ? "./models/Reginox-solid.glb" : "./models/Reginox.glb");
-
-    return (
-        <group
-            name="sink-bowl-group"
-            {...props}
-            dispose={null}
+  return (
+    <group name='sink-bowl-group' {...props} dispose={null}>
+      {tableTopMaterial.name !== "inox" && (
+        <mesh
+          name='sink-bowl-mesh'
+          castShadow
+          receiveShadow
+          geometry={nodes.Reginox.geometry}
+          position={[0, 0.782, 0.242]}
         >
-            {tableTopMaterial.name !== "inox" &&
-
-                <mesh
-                    name="sink-bowl-mesh"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Reginox.geometry}
-                    position={[0, 0.782, 0.242]}
-                >
-                    <NewMaterial
-                        type={"accent"}
-                        ralExclude={true}
-                    />
-                </mesh>
-            }
-            {tableTopMaterial.name === "inox" &&
-                <mesh
-                    name="sink-bowl-solid-mesh"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Reginox002.geometry}
-                    position={[0, 0.765, 0.242]}
-                >
-                    <NewMaterial
-                        type={"tableTop"}
-                        ralExclude={true}
-                    />
-                </mesh>
-            }
-        </group>
-    );
+          <NewMaterial type={"accent"} ralExclude={true} />
+        </mesh>
+      )}
+      {tableTopMaterial.name === "inox" && (
+        <mesh
+          name='sink-bowl-solid-mesh'
+          castShadow
+          receiveShadow
+          geometry={nodes.Reginox002.geometry}
+          position={[0, 0.765, 0.242]}
+        >
+          <NewMaterial type={"tableTop"} ralExclude={true} />
+        </mesh>
+      )}
+      <mesh
+        rotation={[-Math.PI / 2, 0, 0]}
+        position={[0, 0.77, 0.1]}
+        scale={[0.3, 0.3, 0.3]}
+      >
+        <planeGeometry args={[1, 1]} />
+        <meshStandardMaterial color='black' />
+      </mesh>
+    </group>
+  );
 }
 
 useGLTF.preload("./models/Reginox.glb");
