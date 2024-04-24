f89641f6fe29fd12d0abcc28226b9b0088dbaae2
ArneSamson <arnesamson007@gmail.com>
2024-04-24 15:38:57 +0200

    feat: give stools more ralistisc metal stand

--- src/components/kitchen/accessoires/Stool.jsx
+++ src/components/kitchen/accessoires/Stool.jsx
@@ -1,81 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { useGLTF } from '@react-three/drei'
-import { useSpring, a, easings } from '@react-spring/three'
+import React, { useEffect, useState } from "react";
+import { MeshStandardMaterial } from "three";
+import { useGLTF } from "@react-three/drei";
+import { useSpring, a, easings } from "@react-spring/three";
 
-import useConfigStore from '../../../store/useConfigStore';
+import useConfigStore from "../../../store/useConfigStore";
 
 export default function Stool({ props }) {
+  const { showChairs } = useConfigStore((state) => ({
+    showChairs: state.showChairs,
+  }));
 
+  const { nodes, materials } = useGLTF("/models/stool.glb");
 
-    const {
-        showChairs,
-    } = useConfigStore(
-        state => ({
-            showChairs: state.showChairs
-        })
-    );
+  const [isVisible, setIsVisible] = useState(showChairs);
 
-    const { nodes, materials } = useGLTF("/models/stool.glb");
+  const [stoolScale, setStoolScale] = useSpring(() => ({
+    scale: showChairs ? [1, 1, 1] : [0.01, 0.01, 0.01],
+    config: {
+      duration: 250,
+      easing: easings.easeOutExpo,
+    },
+    reverse: !showChairs,
+    onStart: () => {
+      setIsVisible(true);
+    },
+    onRest: () => {
+      if (showChairs === false && isVisible === true) {
+        setIsVisible(false);
+      }
+    },
+  }));
 
-    const [isVisible, setIsVisible] = useState(showChairs);
+  useEffect(() => {
+    setStoolScale({
+      scale: showChairs ? [1, 1, 1] : [0.01, 0.01, 0.01],
+    });
+  }, [showChairs, setStoolScale]);
 
-    const [stoolScale, setStoolScale] = useSpring(() => ({
-        scale: showChairs ? [1, 1, 1] : [0.01, 0.01, 0.01],
-        config: {
-            duration: 250,
-            easing: easings.easeOutExpo
-        },
-        reverse: !showChairs,
-        onStart: () => {
-            setIsVisible(true);
-        },
-        onRest: () => {
-            if (showChairs === false && isVisible === true) {
-                setIsVisible(false);
-            }
-        }
-    }));
+  const chairMetal = new MeshStandardMaterial({
+    color: "#AA9E8C",
+    metalness: 1,
+    roughness: 0.3,
+    envMapIntensity: 0.5,
+  });
 
-    useEffect(() => {
-        setStoolScale({
-            scale: showChairs ? [1, 1, 1] : [0.01, 0.01, 0.01],
-        });
-    }, [showChairs, setStoolScale]);
-
-    return isVisible ? (
-        <a.group
-            name="stool-group-container"
-            scale={stoolScale.scale}
-            {...props}
-            dispose={null}
-        >
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.stool.geometry}
-                material={materials.velvet}
-            >
-                <mesh
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.stand_1.geometry}
-                    material={materials['Material.001']}
-                />
-                <mesh
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.stand_2.geometry}
-                    material={materials['Material1 metal']}
-                />
-                <mesh
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.stand_3.geometry}
-                    material={materials['Material2 plastic']}
-                />
-            </mesh>
-        </a.group>
-    ) : null;
+  return isVisible ? (
+    <a.group
+      name='stool-group-container'
+      scale={stoolScale.scale}
+      {...props}
+      dispose={null}
+    >
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.stool.geometry}
+        material={materials.velvet}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.stand_2.geometry}
+          //   material={materials["Material1 metal"]}
+          material={chairMetal}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.stand_3.geometry}
+          material={materials["Material2 plastic"]}
+        />
+      </mesh>
+    </a.group>
+  ) : null;
 }
 
-useGLTF.preload('/models/stool.glb')
+useGLTF.preload("/models/stool.glb");
